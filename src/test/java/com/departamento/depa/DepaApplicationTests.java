36a013235ff07729ecb98c96867e086868c2ea86
Christian774963 <nun645321@gmail.com>
2026-05-30 16:59:06 -0500

    primer commit backend spring boot

---
+++ src/test/java/com/departamento/depa/DepaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.departamento.depa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
